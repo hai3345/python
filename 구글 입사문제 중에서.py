2c03037553a34da53136df35fd3e8f79c5928182
hai3345 <39399967+hai3345@users.noreply.github.com>
2021-03-29 14:48:00 +0900

    Update 구글 입사문제 중에서.py

--- 구글 입사문제 중에서.py
+++ 구글 입사문제 중에서.py
@@ -13,4 +13,8 @@ for i in range(1,10001):
 
 result
 
-#https://codingdojang.com/scode/393?answer_mode=hide
+"""https://codingdojang.com/scode/393?answer_mode=hide
+1부터 10,000까지 8이라는 숫자가 총 몇번 나오는가?
+
+8이 포함되어 있는 숫자의 갯수를 카운팅 하는 것이 아니라 8이라는 숫자를 모두 카운팅 해야 한다.
+(※ 예를들어 8808은 3, 8888은 4로 카운팅 해야 함)"""

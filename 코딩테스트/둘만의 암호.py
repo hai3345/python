771dfdbeaf667426cda82fcfaca91bb7d28bb498
hai3345 <hai3345@hanmail.net>
2023-02-13 16:20:03 +0900

    ..

--- 코딩테스트/둘만의 암호.py
+++ 코딩테스트/둘만의 암호.py
@@ -7,7 +7,7 @@ https://school.programmers.co.kr/learn/courses/30/lessons/155652
 
 def solution(s, skip, index):    
     alpha= list(ascii_lowercase)
-    
+    """ 
     for i in range(0,len(skip)):
         skipAlpha = skip[i]
         alpha.remove(skipAlpha)
@@ -22,8 +22,26 @@ def solution(s, skip, index):
             alphaIndex = alphaIndex - len(alpha)
         
         answer = answer + str(alpha[alphaIndex])
+    """
 
-    return answer
+    result = ''
+
+    a_to_z = set(ascii_lowercase)
+    a_to_z -= set(skip)
+    a_to_z = sorted(a_to_z)
+    l = len(a_to_z)
+
+    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
+
+    print(dic_alpha)
+
+    for i in s:
+        result += a_to_z[(dic_alpha[i] + index) % l]
+
+    return result
+
+
+
 
 solution("aukks", 'wbqd', 5)
 
---
+++ 코딩테스트/평행.py
@@ -0,0 +1,18 @@
+"""
+def solution(dots):
+    
+    
+    answer = 0
+    return answer
+
+
+
+dots = [[x1,y1], [x2,y2], [x3,y3], [x4,y4]]
+
+solution(dots)
+
+"""
+
+a = [[2,3], [4,5]]
+
+print(a[0][0])
